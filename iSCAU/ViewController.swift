1a4df13d6fe0dc165437abdbe341ecd4879c2b8f
ECar <ECar@lufuyuan@enjoytech.top>
2015-07-05 16:59:30 +0800

    Initial Commit

---
+++ iSCAU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iSCAU
+//
+//  Created by Rich on 15-7-5.
+//  Copyright (c) 2015年 iSCAU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
